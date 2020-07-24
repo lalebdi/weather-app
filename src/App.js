cb81231421a4c8f3ac2682a530141f6ea9069f7c
lalebdi <loai.alebdi@gmail.com>
2020-07-23 20:06:12 -0400

    Components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Weather from './component/Weather'
 
 function App() {
   return (
     <div className="App">
       <h1>The Weather App</h1>
+      <Weather />
     </div>
   );
 }
